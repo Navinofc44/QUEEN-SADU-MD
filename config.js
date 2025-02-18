8820e8273a88f8504ad9665b8912ea9ad9bb8393
Navinofc44 <Pahasarasanujja@gmail.com>
2025-02-18 20:39:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kWNnRBLR#gNpofiANTDh5IuRVP5x9zCHZ95uIBzZYXsZyL2ug7kA",
+SESSION_ID: process.env.SESSION_ID || "cWVwxQjJ#giJgGQCElLLQFyiQV0-1YI6LUDGXm0-eigtezN2Jv60",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -21,7 +21,7 @@ BOT_NAME: process.env.BOT_NAME || "QUEEN-SADU-MD",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "QUEEN-SADU-MD",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
@@ -49,7 +49,7 @@ ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 // make true for send automatic voices
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 // make true for automatic stickers 
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 // make true or false automatic text reply 
